c711d460a964ecc1e15a47d329c0931ea8a024a8
piction165 <plaes165@gmail.com>
2025-03-12 23:58:42 +0900

    Delete backend directory

--- backend/controllers/userController.js
+++
@@ -1,6 +0,0 @@
-const User = require('../models/User');
-
-exports.getMyPage = async (req, res) => {
-  const user = await User.findById(req.user.id);
-  res.json({ username: user.username, email: user.email });
-};
--- backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-const authMiddleware = require('../middleware/auth');
-
-router.get('/mypage', authMiddleware, userController.getMyPage);
-
-module.exports = router;
